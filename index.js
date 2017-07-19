6d7b21d2314bf8af8860ff17bf0a16d2151a8423
“Alexander <aocsa.cs@gmail.com>
2017-07-19 19:05:56 +0000

    new

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-// node.js, "classic" way:
-var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-var result = md.render('# markdown-it rulezz!');
+$( function() {
+  var evaluated = false;
 
-// node.js, the same, but with sugar:
-var md = require('markdown-it')();
-var result = md.render('# markdown-it rulezz!');
+  let searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.has('sent')  ) {
+      document.getElementById("code").innerHTML = '';
+
+      $.get( "src/sample.js", function( data ) {
+        console.log (data);
+         var notebook = RunKit.createNotebook({
+            // the parent element for the new notebook
+            element: document.getElementById("code"),
+
+            preamble: "var _ = require(\"lodash\")",
+
+            // specify the source of the notebook
+            source: data,
+
+            onResult: function () {
+
+                evaluated = true;
+            }
+        });
+
+          $( "#code" ).dialog( {
+                  show: {
+                    effect: "blind",
+                    duration: 200
+                  },
+                  hide: {
+                    effect: "explode",
+                    duration: 200
+                  },
+                   width: "90%",
+                   maxWidth: "768px",
+                   height: "90%",
+                   minHeight: "250px"
+
+                }
+            );
+      });
+    }
 
 
+
+
+} );
